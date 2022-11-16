4e570368e77b39b1924425f7591bd2e6d9ee6e53
XimenesJu <ju.cx@hotmail.com>
2022-11-15 22:06:26 -0300

    created js functions + generate advice btn working + displaying advices on screen

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const advice = document.getElementById("advice");
-
+const advice = document.getElementById("advice")
+const slipid = document.getElementById("slipid")
 
 async function generateAdvice() {
   var slipAdvice = await fetch('https://api.adviceslip.com/advice');
   var convertedSlipAdvice = await slipAdvice.json();
-  console.log(convertedSlipAdvice);
+  return convertedSlipAdvice;
 }
 
-generateAdvice();
-
-function myAdvice() {
-  document.getElementById("advice").innerHTML = generateAdvice()
-  .then() ;
+async function myAdvice() {
+  var getid = await generateAdvice() 
+  var getadvice = await generateAdvice() 
+  slipid.innerHTML = getid.slip.id
+  advice.innerHTML = getadvice.slip.advice
+  console.log(getadvice.slip.advice)
+  console.log(getid.slip.id)
 }
+
 
 
